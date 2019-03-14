e9fbc4d13f8ca245558ecf039a4bf9337fbc0a98
Philip Schell <philip.schell@fit.fraunhofer.de>
2019-03-14 15:58:59 +0100

    Change Icons

--- Lora-Map/Model/Marker.cs
+++ Lora-Map/Model/Marker.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 
-namespace Fraunhofer.Fit.IoT.LoraMap.Model {
+namespace Fraunhofer.Fit.IoT.LoraMap.Model
+{
   class Marker {
     private readonly XmlDocument svg = new XmlDocument();
 
@@ -22,9 +19,41 @@ namespace Fraunhofer.Fit.IoT.LoraMap.Model {
         if(keyvalue.Length == 2) {
           switch(keyvalue[0].ToLower()) {
             case "name":
-              XmlNodeList node = this.svg.DocumentElement.SelectNodes("//*[local-name()='tspan'][@id='marker-name-text']");
-              if(node.Count == 1) {
-                node.Item(0).InnerText = keyvalue[1];
+              XmlNodeList xmlname = this.svg.DocumentElement.SelectNodes("//*[local-name()='tspan'][@id='marker-name-text']");
+              if(xmlname.Count == 1) {
+                xmlname.Item(0).InnerText = keyvalue[1];
+              }
+              break;
+            case "icon":
+              if(keyvalue[1] == "person") {
+                XmlNodeList xmlicon = this.svg.DocumentElement.SelectNodes("//*[local-name()='defs'][@id='global-def']");
+                if (xmlicon.Count == 1) {
+                  xmlicon.Item(0).InnerXml += "<style type=\"text/css\">#marker-icon #person { display: inline; }</style>";
+                }
+              }
+              break;
+            case "person-org":
+              XmlNodeList xmlpersonorg = this.svg.DocumentElement.SelectNodes("//*[local-name()='defs'][@id='people-def']");
+              if (xmlpersonorg.Count == 1) {
+                xmlpersonorg.Item(0).InnerXml += "<style type=\"text/css\">#person-layer-org #person-layer-org-"+ keyvalue[1] + " { display: inline; }</style>";
+              }
+              break;
+            case "person-funct":
+              XmlNodeList xmlpersonfunct = this.svg.DocumentElement.SelectNodes("//*[local-name()='defs'][@id='people-def']");
+              if (xmlpersonfunct.Count == 1) {
+                xmlpersonfunct.Item(0).InnerXml += "<style type=\"text/css\">#person-layer-funct #person-layer-funct-" + keyvalue[1] + " { display: inline; }</style>";
+              }
+              break;
+            case "person-rang":
+              XmlNodeList xmlpersonrang = this.svg.DocumentElement.SelectNodes("//*[local-name()='defs'][@id='people-def']");
+              if (xmlpersonrang.Count == 1) {
+                xmlpersonrang.Item(0).InnerXml += "<style type=\"text/css\">#person-layer-rang #person-layer-rang-" + keyvalue[1] + " { display: inline; }</style>";
+              }
+              break;
+            case "person-text":
+              XmlNodeList xmlpersontext = this.svg.DocumentElement.SelectNodes("//*[local-name()='tspan'][@id='person-layer-typ-text']");
+              if (xmlpersontext.Count == 1) {
+                xmlpersontext.Item(0).InnerText = keyvalue[1];
               }
               break;
           }
